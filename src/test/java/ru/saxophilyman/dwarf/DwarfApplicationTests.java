bdcea4f3e211c16beef52e10dd79176fe0e0e384
Saxophilyman <zd_@mail.ru>
2025-04-11 13:56:17 +0400

    Init Project fo repeate basic technology

---
+++ src/test/java/ru/saxophilyman/dwarf/DwarfApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.saxophilyman.dwarf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwarfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
